8df71db0ae2b2ef1e029d37752f40d3018734bed
Hemil78 <zanattahemil@gmail.com>
2021-11-07 17:43:44 +0100

    header hero

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,35 @@
 <header>
-    
+    <div class="visa">
+        <div class="container">
+            <span>DC POWER VISA</span>
+            <span>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></span>
+        </div>
+        
+    </div>
+    <div class="container">
+        <div class="box-header">
+            <div class="header-logo">
+                <img src="images/dc-logo.png" alt="logo-DC-comics">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li class="active"><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+            <div class="search-header">
+                <input type="text" name="search" id="search" placeholder="Search">
+                <i class='fas fa-search'></i>
+            </div>
+        </div>
+    </div>
+    <div class="hero"></div>
 </header>
